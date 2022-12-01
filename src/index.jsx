2266189d70a05ab9e66e2bb3ea276edfa42026e2
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-01 19:03:01 +0100

    added navbar

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import ReactDOM from "react-dom";
 import { MainView } from "./components/main-view/main-view";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "./logo.png";
 
 import "./index.scss";
 
@@ -9,6 +11,24 @@ class FavFlixApplication extends React.Component {
   render() {
     return (
       <Container>
+        <Navbar
+          bg="dark"
+          variant="dark"
+          sticky="top"
+          expand="lg"
+          className="navbar"
+        >
+          <Navbar.Brand href="/">
+            <img src={logo} className="logo" /> FavFlix
+          </Navbar.Brand>
+
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <MainView />
       </Container>
     );
